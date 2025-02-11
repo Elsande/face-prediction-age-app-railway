a30e52266ed17871b08fd9280b627a0d09f090e0
elsande <elsandepitra890@gmail.com>
2025-02-11 23:00:07 +0700

    update

--- app.py
+++ app.py
@@ -89,6 +89,6 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Ambil port dari environment variable
+    port = int(os.environ.get("PORT", 5000))  # Ambil port dari environment variable
     print(f"🚀 Server berjalan di port {port}")
     serve(app, host="0.0.0.0", port=port)
